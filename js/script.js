b521fbfd950e1defb59195560f15c7687c4ba22a
ilariamotta <ilaria.motta93@gmail.com>
2025-11-19 12:47:24 +0100

    feat: eventlistener a foto e bottone

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@
 const photoGrid = document.getElementById("polaroid-grid");
 const loadingMan = document.getElementById("loader");
 
+const bigPhoto = document.getElementById("bigPhoto");
+const photoOverlay = document.getElementById("photo-overlay");
+
+const xButton = document.getElementById("close");
+
 
 
 axios.
@@ -14,19 +19,45 @@ axios.
         photoArray.forEach(curPhoto => {
             curPolaroid += `<div class="m-col m-flex m-justify-content-between"><div class="m-card m-flex m-dcol">
                     <div class="m-pin"><img src="./img/pin.svg" alt=""></div>
-                    <div class="m-card-img m-flex m-justify-content-center"><img src="${curPhoto.url}" alt=""></div>
+                    <div class="m-card-img m-flex m-justify-content-center"><img class="photo-polaroid" src="${curPhoto.url}" alt=""></div>
                     <div class="m-card-body">
                         <p class="my-date">${curPhoto.date}</p>
                         <p class="my-description">${curPhoto.title}</p>
                     </div>
                 </div></div>`
+
         });
         photoGrid.innerHTML = curPolaroid;
 
-loader.classList.add("m-dnone");
+        const polaroid = document.querySelectorAll(".photo-polaroid");
 
-photoGrid.classList.remove("m-dnone");
-});
+        polaroid.forEach((element) => {
+            element.addEventListener("click", function () {
+                bigPhoto.src = element.src;
+                photoOverlay.classList.remove("hidden");
+
+                xButton.addEventListener("click", function () {
+                    photoOverlay.classList.add("hidden");
+                })
+
+            })
+        })
+
+        loader.classList.add("m-dnone");
+
+        photoGrid.classList.remove("m-dnone");
+    });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
